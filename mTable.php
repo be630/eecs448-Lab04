16dd616c5e500cc9dee903eb4e01ae44fb2d3ebe
Blaine Edmondson <blaine@ku.edu>
2017-10-01 20:52:19 -0500

    got a 10 by 10 table to print

--- mTable.php
+++ mTable.php
@@ -1,3 +1,21 @@
 <?php
-   echo "<p>Hello, World!</p>";
+  
+	echo "<table>";
+
+		for($r = 1; $r <= 10; $r++)
+		{
+			echo "<tr>";
+			
+			for($c = 1; $c <= 10; $c++)
+			{
+				$populate = $r * $c;
+				echo "<td> $populate </td>";
+			}
+			
+			echo "</tr>";
+			
+			
+		}
+		
+	echo "</table>";
 ?>
